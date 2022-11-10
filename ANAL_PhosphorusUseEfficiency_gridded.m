9285a2fa57c2d548493c6afd65e1c48257420b00
danykakbyrnes <danyka.byrnes@gmail.com>
2022-11-09 22:45:33 -0500

    updating code to convert int32 to singles, peform PUE calculations, and convert back to int32 to save

--- ANAL_PhosphorusUseEfficiency_gridded.m
+++ ANAL_PhosphorusUseEfficiency_gridded.m
@@ -6,20 +6,20 @@ clc, clear
 INPUTfilepath = ['..\..\3 TREND_Nutrients\TREND_Nutrients\OUTPUTS\',...
     'Grid_TREND_P_Version_1\TREND-P Postpocessed Gridded\'];
 OUTPUTfilepath = '..\OUTPUTS\PUE\';
-YEARS = [1930:2017];
+YEARS = 1930:2017;
 
 livestockFolder = 'CropUptake_Agriculture_Agriculture_LU';
 fertilizerFolder = 'Fertilizer_Agriculture_Agriculture_LU';
 cropFolder = 'Lvst_Agriculture_LU';
 
 delete(gcp('nocreate')); % Close any pools that might already be running
-parpool('local',4);
+parpool('local',12);
 
 Rinfo = geotiffinfo([INPUTfilepath,'Lvst_BeefCattle_Agriculture_LU\BeefCattle_1930.tif']);
 
 % calculate PUE and save the tif file
 
-for i = 1:length(YEARS)
+parfor i = 1:length(YEARS)
    
     YEAR_i = YEARS(i);
     %PUE_fert = Crop_var./allFetilizer;
@@ -36,9 +36,17 @@ for i = 1:length(YEARS)
     file_crop_i = dir([INPUTfilepath, livestockFolder,'\*_',num2str(YEAR_i),'.tif']);
     [Crop_i,~] = readgeoraster([INPUTfilepath, livestockFolder,'\',file_crop_i.name]);
     
+    % Convert ints32 to doubles. 
+    Livestock_i = single(Livestock_i);
+    Fertilizer_i = single(Fertilizer_i);
+    Crop_i = single(Crop_i);
+
+    % Calculating PUE with integers. 
     PUE = Crop_i ./Inputs;
     
-    geotiffwrite([OUTPUTfilepath, '\PUE_', num2str(YEAR_i),'.tif'], PUE, ...
+    PUE = PUE*1000;
+
+    geotiffwrite([OUTPUTfilepath, '\PUE_', num2str(YEAR_i),'.tif'], int32(PUE), ...
     georef, 'GeoKeyDirectoryTag',Rinfo.GeoTIFFTags.GeoKeyDirectoryTag, ...
     'TiffTags',struct('Compression',Tiff.Compression.LZW));
 
