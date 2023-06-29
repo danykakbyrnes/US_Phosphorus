df84af40773b6727fc4732b2e0273bcdc2c05281
unknown <danyka.byrnes@gmail.com>
2023-06-29 18:48:42 -0400

    updating code to process the crop to fertilizer ratio

--- ANAL_ClippingCFR_CountyState.py
+++ ANAL_ClippingCFR_CountyState.py
@@ -41,7 +41,7 @@ for filename in os.listdir(raster_folder):
             
             # Create a new column in the shapefile for the raster statistics
             year = filename.split('_')[1].split('.')[0]
-            column_name = f'Median_{year}'  # Create a column name with the year
+            column_name = f'Y{year}'  # Create a column name with the year
             shapefile[column_name] = [stat['median'] for stat in stats]
             
 # Move the geometry column to the end
@@ -51,4 +51,85 @@ shapefile.insert(len(shapefile.columns), 'geometry', geometry_column)
 
 # Save the updated shapefile with the zonal statistics
 output_shapefile = 'CFR_counties.shp'
+shapefile.to_file(raster_folder+output_shapefile)
+
+
+#%% State level analysis
+shapefile_path = 'B:/LabFiles/users/DanykaByrnes/3 TREND_Nutrients/TREND_Nutrients/INPUTS_102122/0 General Data/cb_2017_us_state_5m/cb_2017_us_state_5m_5070.shp'
+# Read the shapefile using geopandas
+shapefile = gpd.read_file(shapefile_path)
+
+# Iterate over each raster in the folder
+for filename in os.listdir(raster_folder):
+    if filename.endswith('.tif'):  # Change the file extension if needed
+        raster_path = os.path.join(raster_folder, filename)
+
+        # Read the raster file using rasterio
+        with rasterio.open(raster_path) as src:
+            # Mask the raster using the shapefile's geometry
+            raster, raster_transform = mask(src, shapefile.geometry, crop=True)
+
+            # Get the statistics for each polygon in the shapefile
+            stats = zonal_stats(
+                shapefile_path,
+                raster[0],
+                affine=raster_transform,
+                nodata=src.nodata,
+                stats=['median']
+                )
+            
+            # Create a new column in the shapefile for the raster statistics
+            year = filename.split('_')[1].split('.')[0]
+            column_name = f'Y{year}'  # Create a column name with the year
+            shapefile[column_name] = [stat['median'] for stat in stats]
+            
+# Move the geometry column to the end
+geometry_column = shapefile.geometry
+shapefile.pop('geometry')
+shapefile.insert(len(shapefile.columns), 'geometry', geometry_column)
+
+# Save the updated shapefile with the zonal statistics
+output_shapefile = 'CFR_state.shp'
+shapefile.to_file(raster_folder+output_shapefile)
+
+#%% Path to the folder containing rasters
+raster_folder = 'B:/LabFiles/users/DanykaByrnes/9 Phosphorus Use Efficiency/OUTPUTS/ManureFertRatio/'
+
+# Path to the shapefile
+shapefile_path = 'B:/LabFiles/users/DanykaByrnes/3 TREND_Nutrients/TREND_Nutrients/INPUTS_102122/0 General Data/CountyShapefiles/US_CountyShapefile_2017.shp'
+
+# Read the shapefile using geopandas
+shapefile = gpd.read_file(shapefile_path)
+
+# Iterate over each raster in the folder
+for filename in os.listdir(raster_folder):
+    if filename.endswith('.tif'):  # Change the file extension if needed
+        raster_path = os.path.join(raster_folder, filename)
+
+        # Read the raster file using rasterio
+        with rasterio.open(raster_path) as src:
+            # Mask the raster using the shapefile's geometry
+            raster, raster_transform = mask(src, shapefile.geometry, crop=True)
+
+            # Get the statistics for each polygon in the shapefile
+            stats = zonal_stats(
+                shapefile_path,
+                raster[0],
+                affine=raster_transform,
+                nodata=src.nodata,
+                stats=['median']
+                )
+            
+            # Create a new column in the shapefile for the raster statistics
+            year = filename.split('_')[1].split('.')[0]
+            column_name = f'Y{year}'  # Create a column name with the year
+            shapefile[column_name] = [stat['median'] for stat in stats]
+            
+# Move the geometry column to the end
+geometry_column = shapefile.geometry
+shapefile.pop('geometry')
+shapefile.insert(len(shapefile.columns), 'geometry', geometry_column)
+
+# Save the updated shapefile with the zonal statistics
+output_shapefile = 'MFR_counties.shp'
 shapefile.to_file(raster_folder+output_shapefile)
