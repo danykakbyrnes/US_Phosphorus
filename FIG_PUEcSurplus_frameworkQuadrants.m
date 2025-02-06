3d29a048fe8b1e6717b8739a543e07081b8cc2cf
Danyka Byrnes <danyka.byrnes@gmail.com>
2025-02-06 18:05:18 -0500

    updating quadrant maps to only include ag land that exists in 198- and 2017; reflect the same data in boxplot and sankey.

--- FIG_PUEcSurplus_frameworkQuadrants.m
+++ FIG_PUEcSurplus_frameworkQuadrants.m
@@ -1,12 +1,13 @@
 clc, clear, close all
 
+%% FIGURE 7: Framework figure
+
 % Aesthetic 
 fontSize_p = 11;
 fontSize_p2 = 8; 
 plot_dim_1 = [400,400,200,150];
 plot_dim_2 = [200,200,215,350];
 plot_dim_alt = [200,200,215,215];
-plot_dim_3 = [200,200,400,200];
 
 mSize = 4; 
 
@@ -15,17 +16,12 @@ colourPalette = [1,102,94;
                  90,180,172;
                  216,179,101]./255;
              
-% colourPalette_extended = [1,102,94;1,102,94;
-%                         216,179,101; 216,179,101; 
-%                         90,180,172; 90,180,172;
-%                         140,81,10; 140,81,10]./255;
-
 % Read in gif files
 INPUTfilepath = '..\INPUTS_103122\';
 OUTPUTfilepath = '..\OUTPUTS\Quadrants\';
 HUC2filepath = '..\OUTPUTS\HUC2\';
 trendINPUTfilepath = ['..\..\3_TREND_Nutrients\TREND_Nutrients\OUTPUT\',...
-    'Grid_TREND_P_Version_1\TREND-P Postpocessed Gridded (2023-11-18)\'];
+    'Grid_TREND_P_Version_1\TREND-P_Postpocessed_Gridded_2023-11-18\'];
 
 % These data (PUE + SURPcumu) has NaN for all cells that are not ag land. 
 % Reading in 2017 data.
@@ -40,12 +36,6 @@ CUMSUMfilepath = '..\OUTPUTS\Cumulative_Phosphorus\CumSum_1980.tif';
 
 [PUE1980,~] = readgeoraster(PUEfilepath); % single
 [CS1980,~] = readgeoraster(CUMSUMfilepath); % single
-
-% Vectorizing the data
-PUE2017_v = PUE2017(:);
-CS2017_v = CS2017(:);
-PUE1980_v = PUE1980(:);
-CS1980_v = CS1980(:);
 
 clear PUE2017 CS2017 PUE1980 CS1980
 
@@ -119,12 +109,6 @@ D = D_cleanedData;
 % gridcells available.
 I = sort(randperm(length(D),ceil(length(D)/100))'); 
 D = D(I,:);
-
-% Create colormaps
-colourPalette = [1,102,94;
-                140,81,10; 
-                90,180,172;
-                216,179,101]./255;
 
 % Getting quadrant numbers
 unQ = unique(D(:,6));
@@ -222,110 +206,3 @@ Q_pct = [sum(D(:,5) == 1)/length(D), sum(D(:,6) == 1)/length(D);
      sum(D(:,5) == 3)/length(D), sum(D(:,6) == 3)/length(D);
      sum(D(:,5) == 4)/length(D), sum(D(:,6) == 4)/length(D)]*100;
 save([OUTPUTfilepath,'QuadrantPct.mat'], 'Q_pct')
-
-%% Finding the distribution of dominant manure inputs (vs. tot fertilizer) 
-% for each quadrant.
-D = D_allData; % D = CS1980, CS2017, PUE1980, PUE2017, Q1980, Q2017
-
-% Read in the 2017 livestock and fertilzier rasters
-YEARS = 1930:2017;
-
-fertilizerFolder = 'Fertilizer_Agriculture_Agriculture_LU\';
-livestockFolder = 'Lvst_Agriculture_LU\';
-
-[LVSTK2017,~] = readgeoraster([trendINPUTfilepath,livestockFolder,...
-                               'Lvst_2017.tif']);
-[FERT2017,~] = readgeoraster([trendINPUTfilepath,fertilizerFolder,...
-                              'Fertilizer_Ag_2017.tif']);
-%[CROP2017,~] = readgeoraster([trendINPUTfilepath,'CropUptake_Agriculture_Agriculture_LU/CropUptake_2017.tif']);
-
-[LVSTK1980,~] = readgeoraster([trendINPUTfilepath,livestockFolder,...
-                                'Lvst_1980.tif']);
-[FERT1980,~] = readgeoraster([trendINPUTfilepath,fertilizerFolder,...
-                                'Fertilizer_Ag_1980.tif']);
-
-LVSTK2017_v = double(LVSTK2017(:));
-FERT2017_v = double(FERT2017(:));
-%CROP2017_v = double(CROP2017(:));
-
-LVSTK1980_v = double(LVSTK1980(:));
-FERT1980_v = double(FERT1980(:));
-
-% Cleaning the data.  D = CS1980, CS2017, PUE1980, PUE2017, Q1980, Q2017
-DisnanIDX = isnan(D(:,3)) + isnan(D(:,4));
-D = D(DisnanIDX == 0, :);
-LVSTK2017_v = LVSTK2017_v(DisnanIDX == 0, :);
-FERT2017_v = FERT2017_v(DisnanIDX == 0, :);
-LVSTK1980_v = LVSTK1980_v(DisnanIDX == 0, :);
-FERT1980_v = FERT1980_v(DisnanIDX == 0, :);
-
-% Removing the boxes that have no quadrant in 2018
-Lvsk_Fert_Quadrant = [LVSTK2017_v./(FERT2017_v+LVSTK2017_v), D(:,6),... 
-                      repmat(2017,size(D,1),1);
-                      LVSTK1980_v./(FERT1980_v+LVSTK1980_v), D(:,5),...
-                      repmat(1980,size(D,1),1)];
-
-Lvsk_Fert_Quadrant =  array2table(Lvsk_Fert_Quadrant, 'VariableNames', {'LvstkFertFract','Q','QYear'});
-save([OUTPUTfilepath,'Lvstk_Fert_Ratio_Grid.mat'], 'Lvsk_Fert_Quadrant')
-
-% Creating the boxplot panel for quadrant input distribution (figure 6).
-% 2017
-figure(1)
-for i = 1:4
-    sLvsk_Fert_Quadrant= Lvsk_Fert_Quadrant(Lvsk_Fert_Quadrant.Q == i,:);
-    b = boxchart(sLvsk_Fert_Quadrant.Q,...
-        sLvsk_Fert_Quadrant.LvstkFertFract,'MarkerStyle',...
-        'none','BoxFaceColor',colourPalette(i,:),'GroupByColor',...
-        sLvsk_Fert_Quadrant.QYear);
-    hold on
-end
-
-box on
-set(gca,'FontSize',fontSize_p,'LineStyleOrderIndex',3,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'k','k','k'});
-set(gcf,'position',plot_dim_1)
-
-set(gca,'XColor',[0,0,0])
-set(gca,'YColor',[0,0,0])
-set(gca,'ZColor',[0,0,0])
-
-ylim([0,1.05])
-xlim([1,4])
-xticks([1, 1.5, 2, 2.5,3, 3.5, 4])    
-xticklabels({'1980','2017','1980','2017','1980','2017','1980','2017'})
-ylabel('% Manure of Inputs')
-
-close all
-
-unyears = unique(Lvsk_Fert_Quadrant.QYear);
-for j = 1:2
-    subplot(1,2,j)
-    Lvsk_Fert_Quadrant_j = Lvsk_Fert_Quadrant(Lvsk_Fert_Quadrant.QYear == unyears(j),:);
-    for i = 1:4
-        
-        sLvsk_Fert_Quadrant = Lvsk_Fert_Quadrant_j(Lvsk_Fert_Quadrant_j.Q == i,:);
-        b_1980 = boxchart(sLvsk_Fert_Quadrant.Q,...
-            sLvsk_Fert_Quadrant.LvstkFertFract*100,'MarkerStyle',...
-            'none','BoxFaceColor',colourPalette(i,:));
-        hold on
-    
-        oLvsk_Fert_Quadrant = Lvsk_Fert_Quadrant_j(Lvsk_Fert_Quadrant_j.Q ~= i,:);
-        pv(i) = ranksum(sLvsk_Fert_Quadrant.LvstkFertFract, oLvsk_Fert_Quadrant.LvstkFertFract);
-    end
-    box on
-    set(gca,'FontSize',fontSize_p,'LineStyleOrderIndex',3,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'k','k','k'});
-    set(gca,'XColor',[0,0,0])
-    set(gca,'YColor',[0,0,0])
-    set(gca,'ZColor',[0,0,0])
-    
-    ylim([0,105])
-    xticks([1,2,3,4])
-    xticklabels({'Q1','Q2','Q3','Q4'})
-end
-subplot(1,2,1)
-ylabel('% Manure of Inputs')
-yticks([0, 25, 50, 75, 100])
-subplot(1,2,2)
-yticks([])
-set(gcf,'position',plot_dim_3)
-Figfolderpath = [OUTPUTfilepath,'Q_Boxplot_',datestr(datetime,'mmddyy'),'.png'];
-print('-dpng','-r600',[Figfolderpath])
