d6babe6f0d78869f260154bd52c6b63a05518278
Danyka Byrnes <danyka.byrnes@gmail.com>
2025-07-05 12:23:01 -0500

    updating script to include PUE. I rewrote the PUE HUC2 clipping code to calculate the proportion of manure input in each HUC2. Since PUE is calculated for each year, I included it with the other components that are also calculated annually, instead of cumulative surplus (calculated 1980 and 2017) and propor. manure input (2017)

--- DATA_HUC2_Surplus_Components.R
+++ DATA_HUC2_Surplus_Components.R
@@ -20,6 +20,7 @@ YEARS = 1930:2017
 INPUT_folders = '9_Phosphorus_Use_Efficiency/INPUTS_051523/'
 OUTPUT_folders = '9_Phosphorus_Use_Efficiency/OUTPUTS/HUC2/'
 PSURPLUS_OUTPUT_folders = '3_TREND_Nutrients/TREND_Nutrients/OUTPUT/Grid_TREND_P_Version_1/TREND-P_Postpocessed_Gridded_2023-11-18/'
+PUE_OUTPUT_folders = '9 Phosphorus Use Efficiency/OUTPUTS/PUE/'
 HUC2_filepath = '0_General_Data/HUC2/'
 
 # Select which gTREND components to be clipped. 
@@ -71,15 +72,64 @@ for (a in 1:length(Components)) {
         MedianHUC2[j,i+1] = median_val
     }
   }
-  colnames(Comp_extc)[1] ="REG"
+  colnames(MeanHUC2)[1] ="REG"
   write.table(MeanHUC2, file = paste0(OUTPUT_folders, 
                                        ComponentsName[a],
                                       '_meanHUC2Components.txt'), 
               row.names = FALSE)
   
-  colnames(MedianHUC2) <- colnames(Comp_extc)
+  colnames(MedianHUC2) <- colnames(MeanHUC2)
   write.table(MedianHUC2, file = paste0(OUTPUT_folders, 
                                         ComponentsName[a],
                                        '_medianHUC2Components.txt'), 
               row.names = FALSE)
+}
+
+## Now processing PUE at the regional scale. 
+
+# Creating empty dataframe to populate
+MeanHUC2 = data.frame()
+MedianHUC2 = data.frame()
+
+for (i in 1:length(YEARS)) {
+  tif_folders = tif_folders = paste0(PUE_OUTPUT_folders, 'PUE_', YEARS[i],'.tif')
+  R = terra::rast(tif_folders)
+    
+    # Removing 0s (PUE doesn't have 0 values, but implementing as a fail safe)
+    temp = values(R)
+    temp = as.matrix(temp)
+    temp[temp == 0] = NA
+    temp = as.data.frame(temp)
+    values(R) = temp
+    
+    for (j in 1:dim(HUC2)[1]) {
+      # Calculating the mean for jth HUC2 watershed
+      mean_val = terra::extract(R, 
+                                HUC2[j,],
+                                fun=mean,
+                                na.rm=TRUE)
+      MeanHUC2[j,1] = HUC2[j,]$REG
+      MeanHUC2[j,i+1] = mean_val[2]
+      
+      # Calculating the median for jth HUC2 watershed
+      HUC2Mask = terra::mask(R,
+                             HUC2[j,],
+                             inverse=FALSE)
+      maskDF = as.data.frame(HUC2Mask, 
+                             na.rm = TRUE) # removes all NaNs
+      median_val = median(unlist(maskDF), 
+                          na.rm = TRUE)
+      
+      MedianHUC2[j,1] = HUC2[j,]$REG
+      MedianHUC2[j,i+1] = median_val
+    }
   }
+  colnames(MeanHUC2)[1] ="REG"
+  write.table(MeanHUC2, file = paste0(OUTPUT_folders, 
+                                      'PUE_meanHUC2_fromgrid'), 
+              row.names = FALSE)
+  
+  colnames(MedianHUC2) <- colnames(MeanHUC2)
+  write.table(MedianHUC2, file = paste0(OUTPUT_folders, 
+                                        'PUE_medianHUC2_fromgrid'), 
+              row.names = FALSE)
